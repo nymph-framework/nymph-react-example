1303c571fbd514177cc59cb7f942145f8dace4f8
ghost <9334746+the-robot@users.noreply.github.com>
2020-03-23 11:19:01 +0800

    init project example

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from nymph import Nymph
+from nymph.staticfiles import StaticFiles
+from nymph.templating import Jinja2Templates
+
+
+# Configure Static and Template instances
+static = StaticFiles(path="/static", directory="./static")
+templates = Jinja2Templates(directory="templates")
+
+app = Nymph()
+app.mount(static)
+
+@app.route("/")
+async def index(req):
+    res = templates.TemplateResponse("index.html")
+    return res
